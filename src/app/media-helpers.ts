4b67df69abafeb5563dd1690c150362c59d89892
Nikola Djukanovic <nikola.djukanovic@rationaletech.com>
2019-09-25 08:23:42 +0200

    fix: siplification

--- src/app/media-helpers.ts
+++ src/app/media-helpers.ts
@@ -1,9 +1,5 @@
 export const getUserMedia = ({ audio, video }) => {
   return new Promise<MediaStream>((resolve, reject) => {
-    navigator.getUserMedia({ audio, video }, stream => {
-      resolve(stream);
-    }, error => {
-      reject(error);
-    });
+    navigator.getUserMedia({ audio, video }, resolve, reject);
   });
 };
